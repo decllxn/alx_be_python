114a82f7a6dbeaea59390ee1e987e71b1f0ff51a
Declan Munene <munene.declan1@gmail.com>
2024-06-18 13:31:05 +0300

    Future age calculator

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Author - Declan Munene
+# Date - 18/06/2024
+
+age = int(input("How old are you? "))
+future_age = age + 27
+
+print("In 2050, you will be " + str(future_age) + " years old.")
