91435ae139e15c94dd76bb1f0f4e8283cccc100e
Declan Munene <munene.declan1@gmail.com>
2024-06-18 13:11:12 +0300

    Simple interest calculator

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# Author - Declan Munene
+# Date - 18/06/2024
+# Description - Simple Interest calculator
+
+principal = 1000
+rate = 0.05
+time = 3
+interest = principal * rate * time
+
+print("The simple interest is: " + str(interest))
