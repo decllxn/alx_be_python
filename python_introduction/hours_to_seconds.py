128f96ff20713f7e6b0a9f2796ff3960a5bf9445
Declan Munene <munene.declan1@gmail.com>
2024-06-18 13:22:06 +0300

    Hours to seconds convertor

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# Author - Declan Munene
+# Date - 18/06/2024
+# Description - Hours to seconds convertor
+
+hours = 2
+seconds = hours * 60 * 60
+
+print(str(hours) + " hour(s) is " + str(seconds) + " seconds.")
