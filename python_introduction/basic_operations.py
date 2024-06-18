25d99b5e43b2e1e8091fe87580467e17108c3114
Declan Munene <munene.declan1@gmail.com>
2024-06-18 13:03:07 +0300

    Python intro

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Author - Declan Munene
+# Date - 18/06/2024
+# Description - Python script that performs basic arithmetic operations with two predefined numbers.
+
+number1 = 10
+number2 = 5
+
+sum1 = number1 + number2
+difference = number1 - number2
+multiplication = number1 * number2
+
+print("Addition of " + str(number1) + " and " + str(number2) + " is " + str(sum1))
+print("Subtraction of " + str(number1) + " and " + str(number2) + " is " + str(difference))
+print("Multiplication of " + str(number1) + " and " + str(number2) + " is " + str(multiplication)) 
