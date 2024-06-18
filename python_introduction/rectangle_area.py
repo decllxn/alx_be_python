36f756fd090fa2c3092d281a89f956f8eff9b1eb
Declan Munene <munene.declan1@gmail.com>
2024-06-18 13:16:48 +0300

    Area of a triangle

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# Author - Declan Munene
+# Date - 18/06/2024
+# Description - Area of rectangle calculator
+
+length = 10
+width = 5
+area = length * width
+
+print("The area of the rectangle is: " + str(area))
