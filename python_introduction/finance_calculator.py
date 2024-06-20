521a599c5bb7751976fcee67315576848206b0a6
Declan Munene <munene.declan1@gmail.com>
2024-06-20 10:47:34 +0300

    Monthly finace calculator

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 # Author - Declan Munene
-# Date - 18/06/2023
+# Date - 20/06/2023
 # Description - Personal finance calculator
 
+monthly_income = int(input("Enter your monthly income: "))
+monthly_expenses = int(input("Enter your total monthly expenses: "))
 
+monthly_savings = monthly_income - monthly_expenses
+projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+
+print("Your monthly savings are $" + str(monthly_savings))
+print("Projected savings after one year, with interest, is: $" + str(projected_savings))
