c88f85357135235d6d16c5e8dea7d7b389159c31
Declan Munene <munene.declan1@gmail.com>
2024-06-30 17:40:15 +0300

    Hot fix

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -3,7 +3,7 @@
 # Date - 30/6/2024
 # Description - Creating a multiplication table
 
-number = int(input("Enter a number to see the multiplication table: "))
+number = int(input("Enter a number to see its multiplication table: "))
 
 for i in range(1, 11):
     result = number * i
