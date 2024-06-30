2afba3345b7b38d85e49e87df95c592b19bb30d5
Declan Munene <munene.declan1@gmail.com>
2024-06-30 17:02:20 +0300

    Hot fix

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -3,13 +3,13 @@
 # Date - 24/06/2024
 # Description - Script that provides clothing recomendations based # on weather conditions
 
-weather_today = str(input("What\'s the weather like today? (sunny/rainy/cold): "))
+weather_today = input("What\'s the weather like today? (sunny/rainy/cold): ")
 
-if weather_today.lower() == "sunny":
+if weather_today == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif weather_today.lower() == "rainy":
+elif weather_today == "rainy":
     print("Don\'t forget your umbrella and a raincoat.")
-elif weather_today.lower() == "cold":
+elif weather_today == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don\'t have recommendations for this weather.")
