f40477e4e1cdc095ce393bd8ec1bca14fc11ae7e
Declan Munene <munene.declan1@gmail.com>
2024-07-07 18:54:30 +0300

    shopping list manager

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# Author - Declan Munene
+# Date - 7/7/2024
+# Description - A script that creates a simple shopping list manager
+
+
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+    
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            item = input("Enter item to add: ")
+            shopping_list.append(item)
+            pass
+        elif choice == '2':
+            item = input("Enter item to remove: ")
+            shopping_list.remove(item)
+            pass
+        elif choice == '3':
+            print(shopping_list)
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice, Please try again.")
+
+if __name__ == "__main__":
+    main()
