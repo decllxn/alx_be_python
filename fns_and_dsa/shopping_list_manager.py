fe327d20938b50d30d8097d0f93a86b9c02d4871
Declan Munene <munene.declan1@gmail.com>
2024-07-07 18:56:27 +0300

    Hot fix!

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -18,7 +18,7 @@ def main():
         choice = input("Enter your choice: ")
 
         if choice == '1':
-            item = input("Enter item to add: ")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
             pass
         elif choice == '2':
