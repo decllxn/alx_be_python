d76da45c68dbf00df2686259c31e5ec9846bd5a8
Declan Munene <munene.declan1@gmail.com>
2024-07-13 21:19:13 +0300

    Hot fix

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -3,21 +3,24 @@
 # Date - 13/7/2024
 # Description - A script that manages a bank account
 
+# bank_account.py
+
 class BankAccount:
-    def __init__(self, account_balance=0): # Default value set to zero
+    def __init__(self, account_balance=0):  # Default value set to zero
         self.account_balance = account_balance
 
     def deposit(self, amount):
-        self.amount = amount
-        return self.account_balance += self.amount
+        if amount > 0:
+            self.account_balance += amount
+            return True
+        return False
 
     def withdraw(self, amount):
-        if self.account_balance >= amount:
+        if 0 < amount <= self.account_balance:
             self.account_balance -= amount
             return True
-        else:
-            return False
+        return False
 
-    def display_balance(self, result):
-        self.result = result
-        return f"Your account balance is: {self.account_balance}"
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance}")
+
