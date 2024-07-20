259bec4446345383fa7d0b68503ca7ae66b1022a
Declan Munene <munene.declan1@gmail.com>
2024-07-20 16:34:23 +0300

    Methods

---
+++ oop/class_static_methods_demo.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Author - Declan Munene
+# Date - 19/07/2024
+# Description - A script taht defines a class to perform various calculations
+
+class Calculator:
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @classmethod
+    def multiply (cls, a, b):
+        print(calculation_type)
+        return a * b
+
+    def __init__(self, calculation_type):
+        self.calculation_type = calculation_type
